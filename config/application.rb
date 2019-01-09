bd50fd0c13560897ce3f4f9ad69146d746e5b4a0
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2019-01-09 12:00:50 +0100

    Add rack-cors

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,24 @@ module EuroTyper
         require_dependency file
       end
     end
+
     ISO3166.configure do |config|
       config.locales = [:en, :pl]
+    end
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+                 headers: :any,
+                 methods: [
+                   :get,
+                   :post,
+                   :options,
+                   :patch,
+                   :delete
+                 ]
+      end
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
