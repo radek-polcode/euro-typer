0cde89d1d20adae27609587d0a4dca01f8743dbd
Radoslaw Stolarski <radoslaw.stolarski@polcode.net>
2020-05-15 14:28:47 +0200

    Update to rails 6.0

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,19 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,7 +22,19 @@ Bundler.require(*Rails.groups)
 module EuroTyper
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
+    config.debug_exception_response_format = :api
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+
     config.i18n.default_locale = :pl
     # config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << Rails.root.join('services')
@@ -22,10 +47,6 @@ module EuroTyper
     ISO3166.configure do |config|
       config.locales = [:en, :pl]
     end
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
 
     config.middleware.use Rack::Cors do
       allow do
@@ -36,8 +57,5 @@ module EuroTyper
           methods: [:get, :post, :patch, :options, :delete, :put]
       end
     end
-
-    config.api_only = true
-    config.debug_exception_response_format = :api
   end
 end
