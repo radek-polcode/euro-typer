79599ce36573d34c83a11399fae5329c1a298ce4
Radoslaw Stolarski <radoslaw.stolarski@polcode.net>
2020-05-15 15:11:11 +0200

    Add asking for branch during deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :keep_releases, 5
 set :linked_files, %w{config/database.yml}
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
